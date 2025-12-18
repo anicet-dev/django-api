d35bf46929009a88dd7ca13e5ff3ed3d219ac22b
anicet-dev <ton-email@gmail.com>
2025-12-18 11:19:54 +0100

    Fix database config for Render

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 from datetime import timedelta
+import os
 import dj_database_url
 
 # ==================================================
